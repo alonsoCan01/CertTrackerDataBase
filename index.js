0561247b171ecba282aa67dd2aab61ad453c0679
alonsoCan01 <alonso.canedo@neduesic.com>
2021-09-14 15:58:59 -0400

    template changes

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const AWS = require("aws-sdk");
-const { v4: uuidv4 } = require("uuid"); //Random id generation
+//const { v4: uuidv4 } = require("uuid");
 AWS.config.update({ region: "us-east-2" });
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-const certId = uuidv4();
-
-console.log(certId);
+//const certId = uuidv4();
 
 //Add item to table
 
 var params = {
-  TableName: "amplify-certifications",
+  TableName: "alchemy-certifications",
   Item: {
-    Email: "jane.doe@neudesic.com",
-    Cert_ID: `${certId}`,
-    Emp_name: "Jane Doe",
-    Cert_Provider: "Azure",
-    Cert_Name: "AZ-204",
-    Date_Recieved: "01-05-2020",
+    Email: "bob.baker@neudesic.com",
+    Cert_Name: "Solutions Architect",
+    Emp_Name: "Bob Baker",
+    Cert_Provider: "AWS",
+    Date_Recieved: ["2020-12-20"],
+    Date_Expired: ["2023-12-20"],
   },
 };
 
@@ -32,16 +30,33 @@ dynamodb.put(params, function (err, data) {
 // const tempEmpId = "6f8503ce-5d02-461f-a27c-292aea1ef378";
 // const tempCertId = "6f8503ce-5d02-461f-a27c-292aea1ef378";
 
+//Update w/list append
+
 // var params = {
-//   TableName: "Certifications",
+//   TableName: "alchemy-certifications",
 //   Key: {
-//     Emp_ID: `${tempEmpId}`,
-//     Cert_ID: `${tempCertId}`,
+//     Email: "joe.schmoe@neudesic.com",
+//     Cert_Name: "AZ-900",
 //   },
-//   UpdateExpression: "set #Date_Recieved = :Date_Received",
-//   ExpressionAttributeNames: { "#Date_Recieved": "Date_Recieved" },
+//   UpdateExpression:
+//     "SET #dateRecieved = list_append(#dateRecieved, :Date_Received)", //"set #Date_Recieved = :Date_Received",
+//   ExpressionAttributeNames: { "#dateRecieved": "Date_Recieved" },
 //   ExpressionAttributeValues: {
-//     ":Date_Received": "08-31-2020",
+//     ":Date_Received": ["2020-12-06"],
+//   },
+// };
+
+// var params = {
+//   TableName: "alchemy-certifications",
+//   Key: {
+//     Email: "joe.schmoe@neudesic.com",
+//     Cert_Name: "AZ-900",
+//   },
+//   UpdateExpression:
+//     "SET #dateExpired = list_append(#dateExpired, :Date_Expired)", //"set #Date_Expired = :Date_Expired",
+//   ExpressionAttributeNames: { "#dateExpired": "Date_Expired" },
+//   ExpressionAttributeValues: {
+//     ":Date_Expired": ["2021-05-06"],
 //   },
 // };
 
